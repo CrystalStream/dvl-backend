aa1825dd43e037cedadcba36834949faa0bf7de2
Christofer Flores <cristofer.flort@gmail.com>
2019-01-20 10:11:23 -0600

    Add the functionality to update the price per hour

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
+const session = require('express-session');
+const flash = require('express-flash')
 const logger = require('morgan')
 const dayjs = require('dayjs')
 require('dayjs/locale/es')
@@ -20,6 +22,8 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(session({ cookie: { maxAge: 60000 }, secret: 'XQWESFWEGRGERG', resave: false, saveUninitialized: false}));
+app.use(flash())
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/src/views/'));
@@ -27,5 +31,15 @@ app.set('views', path.join(__dirname, '/src/views/'));
 app.use('/', indexRouter)
 app.use('/admin', adminRouter)
 app.use('/api', ticketsRouter)
+
+// 404
+app.use((req, res, next) => {
+  res.status(404).send('Sorry can\'t find that!')
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 module.exports = app
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const ConfigsController = require('../controllers/configsController')
 
 /* GET admin home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin/dashboard', { title: 'Express' })
-})
+router.get('/', ConfigsController.renderForm)
+
+router.post('/config', ConfigsController.updateConfiguration)
 
 module.exports = router
